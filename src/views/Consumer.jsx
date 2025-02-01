e4d90351ecb810454264cf1f39df93e3ae99ba8e
Sumit chauhan <sumitchauhan9807666@gmail.com>
2025-02-01 13:03:18 +0530

    asd

--- src/views/Consumer.jsx
+++ src/views/Consumer.jsx
@@ -42,9 +42,7 @@ function Consumer() {
   }
 
 
-  const showCOnsumers = () => {
-    console.log(consumer)
-  }
+  
 
 
   const consume = async (kind) => {
@@ -135,9 +133,7 @@ function Consumer() {
 				<button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={runTest}>
 					test
 				</button>
-				<button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={showCOnsumers}>
-					show Consumers
-				</button>
+				
 			</div>
 		</>
   )
