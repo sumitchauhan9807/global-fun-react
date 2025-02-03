e44863768d7846481d2c6e2b21be65e83d033ffa
Sumit chauhan <sumitchauhan9807666@gmail.com>
2025-02-03 16:34:51 +0530

    wip

--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const PRODUCTION = true
-const url = PRODUCTION ? 'https://api.globalfun.cam/graphql' : 'http://localhost:4000/graphql'
+const url = PRODUCTION ? 'https://sfu.globalfun.cam/graphql' : 'http://localhost:4000/graphql'
 const client = new ApolloClient({
   uri: url,
   cache: new InMemoryCache(),
